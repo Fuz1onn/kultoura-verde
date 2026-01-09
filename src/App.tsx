73f653a6830075c3353f18d49c282a2d3da7444d
Khalil Acebuche <khalilacebuche17@gmail.com>
2026-01-10 02:18:30 +0800

    File path adjustment 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import Header from "@/components/layout/Header";
 import Home from "@/pages/Home";
 import Footer from "./components/layout/Footer";
+import backgroundImg from "/src/assets/images/background.jpg";
 
 function App() {
   return (
     <div
       className="min-h-screen bg-cover bg-center bg-no-repeat text-white"
       style={{
-        backgroundImage: "url('/src/assets/images/background.jpg')",
+        backgroundImage: backgroundImg ? `url(${backgroundImg})` : "none",
       }}
     >
       <Header />
